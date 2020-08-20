8b8acecd8ab60d16b78980cd9765b6f1049266a1
Nikk Ogasa <ntogasa@gmail.com>
2020-08-19 22:24:52 -0700

    In apps.plan.models change Plan.name to Plan.title, and add Plan.date_published attribute

--- apps/plan/models.py
+++ apps/plan/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class Plan(models.Model):
-    name = models.CharField(max_length=200, default='no name')
+    title = models.CharField(max_length=200, default='no title')
     leader = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=200, default='no description')
+    date_published = models.DateField(default=timezone.now)
+
 
 
