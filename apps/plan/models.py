62f182172aa2230f9b6c6d0780c262c1922f694d
Nikk Ogasa <ntogasa@gmail.com>
2020-08-20 00:16:06 -0700

    In apps.Plan.models add attributes to Plan model class and create Task model class

--- apps/plan/models.py
+++ apps/plan/models.py
@@ -4,11 +4,25 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, default='no description')
+    plan = models.ForeignKey('Plan', on_delete=models.CASCADE)
+
+
 class Plan(models.Model):
     title = models.CharField(max_length=200, default='no title')
     leader = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=200, default='no description')
     date_published = models.DateField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = "Plans"       # we will see this name in the admin page
+
+    def __str__(self):
+        """When referred to as a string, return self.title"""
+        return self.title
+
 
 
 
